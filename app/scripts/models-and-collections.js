8e1371300f42a707f69c38fc4b54e834705d786e
Whitney Williams <wweliz@gmail.com>
2014-06-27 13:44:15 -0400

    initial commit for webapp files

---
+++ app/scripts/models-and-collections.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//initializing Parse
+	//1st arg: App ID; 2nd arg: JavaScript key
+Parse.initialize('VAcV8xNG0NX9Px18WaMwBdxsGyH7CaCk6990BJPn', '2r1eNgxSHNsw77uJdMUUG1ATTzAOgiTYYtImdoXQ');
+
+	//Reminder: when you add somthing to a Parse collection,
+	//an instance of that Parse object is automatically created
+
+// MOUSE MODEL ////////////////////////////////////////////////////
+var Mouse = Parse.Object.extend({
+	className: 'Mouse',
+	defaults: { found	: false }
+});
+
+// MOUSE COLLECTION ////////////////////////////////////////////////
+var MouseCollection = Parse.Collection.extend({
+	model: Mouse
+});
+
+var mice = new MouseCollection;
+
+// PHOTO MODEL ////////////////////////////////////////////////////
+var Photo = Parse.Object.extend({
+	className: 'Photo',
+	defaults: { caption	: '' }
+});
+
+// PHOTO COLLECTION ////////////////////////////////////////////////
+var PhotoCollection = Parse.Collection.extend({
+	model: Photo
+});
+
+var photos = new PhotoCollection;
+
+// USER MODEL ////////////////////////////////////////////////////
+var User = Parse.Object.extend({
+	className: 'User',
+});
+
+// USER COLLECTION ////////////////////////////////////////////////
+var UserCollection = Parse.Collection.extend({
+	model: User
+});
+
+var users = new UserCollection;
+
+//////////////////////////////////////////////////////////////////////////
+console.log('models-and-collections.js file loaded');
---
+++ app/scripts/users.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// CREATING A USER INSTANCE //////////////////////////////////////////////
+// var user = new Parse.User();
+// 	user.set("username",	"my name");
+// 	user.set("password",	"my pass");
+// 	user.set("email",			"email@example.com");
+
+// 	user.set("photos",		"photosArray");
+// 	user.set("mice",			"miceArray");
+
+// USER SIGN UP FUNCTION /////////////////////////////////////////////////
+// user.signUp(null, {
+//   success: function(user) {
+//     console.log('User is logged in.');
+//   },
+//   error: function(user, error) {
+//   	console.log('User was not logged in.');
+//     // Show the error message somewhere and let the user try again.
+//     alert('Error: Sign up failed.' + error.code + ' ' + error.message);
+//     //redirect user back to signup page
+//   }
+// });
+
+// USER LOG IN FUNCTION //////////////////////////////////////////////////
+// Parse.User.logIn("myname", "mypass", {
+//   success: function(user) {
+//     // Do stuff after successful login.
+//   },
+//   error: function(user, error) {
+//     // The login failed. Check error to see why.
+//   }
+// });
+
+
+// SETTING THE CURRENT USER //////////////////////////////////////////////
+// var currentUser = Parse.User.current();
+// if (currentUser) {
+//     // do stuff with the user
+// } else {
+//     // show the signup or login page
+// }
+
+// CLEARING THE CURRENT USER & LOGGING OUT ///////////////////////////////
+// Parse.User.logOut();
+// var currentUser = Parse.User.current();  // this will now be null
+
+
+
+
+//email verification
+
+
+
+//////////////////////////////////////////////////////////////////////////
+console.log('users.js file loaded');
