3cc15fca54a3ef2db865872935a7413c58f4c901
Whitney Williams <wweliz@gmail.com>
2014-07-04 02:19:28 -0400

    working on tracking current user position; need to fix if/then conditions of function

--- app/scripts/geo.js
+++ app/scripts/geo.js
@@ -14,7 +14,7 @@ function checkGeoSuport() {
 
 checkGeoSuport();
 
-// DETERMINING DEVICE LOCATION ///////////////////////////////////////////
+// SAVING USER LOCATION TO PARSE /////////////////////////////////////////
 
 //there are two methods to get the current position of the device:
 	//navigator.geolocation.getCurrentPosition()
@@ -30,13 +30,22 @@ checkGeoSuport();
 													// type: Boolean
 													// default: false
 
-//defines the success callback
-function geoSuccess(position) {
-	var latitude = position.coords.latitude;
-		console.log('Latitude: ', latitude);
 
-	var longitude = position.coords.longitude;
-		console.log('Longitude: ', longitude);
+
+// //defines the success callback
+function geoSuccess(position) {
+	var currentUser = Parse.User.current();
+	var userLatitude = position.coords.latitude;
+	var userLongitude = position.coords.longitude;
+	var point = new Parse.GeoPoint({latitude: userLatitude, longitude: userLongitude});
+
+	currentUser.add({
+		userGeo: point
+	})
+
+	currentUser.save();
+
+	console.log('Current user location is latitude ' + currentUser.attributes.userGeo.latitude + ' longitude '+ currentUser.attributes.userGeo.longitude + '.');
 }
 
 //defines the failure callback
@@ -44,5 +53,24 @@ function geoError() {
 	console.log('Your location could not be determined.');
 }
 
-//passes the success and failure callbacks through the function
-navigator.geolocation.watchPosition(geoSuccess, geoError, {enableHighAccuracy: true});
+function trackUserLocation() {
+	var currentUser = Parse.User.current();
+
+	// if there is a currently logged in user...
+//	if (currentUser) {
+		//passes the success and failure callbacks through the watchPosition function
+			navigator.geolocation.watchPosition(geoSuccess, geoError, {enableHighAccuracy: true});
+	// if there is NOT a currently logged in user...
+	// } else {
+	// 	console.log('Cannot track user location before log in.')
+	// }
+}
+
+//calls the trackUserLocation function
+trackUserLocation();
+
+//need todefine closestMouse
+//<h4> LAT <%= closestMouse.attributes.mouseGeopoint.latitude %>, LONG <%= closestMouse.attributes.mouseGeopoint.longitude %> </h4>
+
+// var img = new Image();
+//     img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
