259e1a05e85149f2fcdff2440f94154f6430294f
Whitney Williams <wweliz@gmail.com>
2014-07-10 19:26:18 -0400

    moving the trackUserLocation function and its callbacks to the router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,6 +1,10 @@
 /* global Parse, _, currentUser */
 'use strict';
 
+// DEFINING UNIVERSAL VARIABLES //////////////////////////////////////////
+var nearbyMice;
+var nearestMouse;
+
 // THE APP ROUTER ////////////////////////////////////////////////////////
 var AppRouter = Parse.Router.extend({
 	routes: {
@@ -27,6 +31,7 @@ var AppRouter = Parse.Router.extend({
 
 		// if there is a currently logged in user...
 		if (currentUser) {
+			this.trackUserLocation();
 			//router redirects to the UserHomeView
 			this.navigate('userview', {trigger: true});
 
@@ -52,6 +57,71 @@ var AppRouter = Parse.Router.extend({
 			// no native support; maybe try a fallback?
 			alert('This device does not support geolocation.');
 			console.log('This device does not support geolocation.');
+		}
+	},
+
+	////////////////////////////////////////////////////////////////////////
+	// TRACKING THE CURRENT USER'S LOCATION ////////////////////////////////
+
+	//there are two methods to get the current position of the device:
+		//navigator.geolocation.getCurrentPosition()
+				//returns the device position ONCE when called
+		//navigator.geolocation.watchPosition()
+				//returns the device position EACH TIME the device position changes
+
+	//both take 3 arguements:
+		// first arg:						success callback [geoSuccess]
+		// second arg: 					failure callback [geoError]
+		// optional third arg:	PositionOptions object
+														// property:	enableHighAccuracy
+														// type: Boolean
+														// default: false
+
+	////////////////////////////////////////////////////////////////////////
+	// DEFINING THE SUCCESS CALLBACK ///////////////////////////////////////
+	geoSuccess: function(position) {
+		var currentUser = Parse.User.current();
+		var userLatitude = position.coords.latitude;
+		var userLongitude = position.coords.longitude;
+		var point = new Parse.GeoPoint({latitude: userLatitude, longitude: userLongitude});
+
+		//"nearbyMice" is a collection of mice that are within a certain radius
+			//nearbyMice.min is the mouse with the shortest distance to the user
+		nearestMouse = nearbyMice.min(function(mouse){
+		  //passing the user location and the nearbyMice collection through the getDistance function
+		  return getDistance(Parse.User.current().get('userGeo'), mouse.get('mouseGeopoint')) * 0.000621371;
+		})	
+
+		console.log('The nearest mouse is ' + nearestMouse.attributes.mouseName + '.')
+
+		// currentUser.set({
+		// 	userGeo: point
+		// });
+
+		// currentUser.save();
+		// console.log('Current user location saved. Latitude: ' + currentUser.attributes.userGeo.latitude + '. Longitude: '+ currentUser.attributes.userGeo.longitude + '.');
+	},
+
+	////////////////////////////////////////////////////////////////////////
+	// DEFINING THE FAILURE CALLBACK ///////////////////////////////////////
+	geoError: function() {
+		alert('Your location could not be determined.');
+		console.log('Your location could not be determined.');
+	},
+
+	////////////////////////////////////////////////////////////////////////
+	// TRACKING THE CURRENT USER'S LOCATION ////////////////////////////////
+	trackUserLocation: function() {
+		var currentUser = Parse.User.current();
+
+		// if there is a currently logged in user...
+		if (currentUser) {
+			//passes the success and failure callbacks through the watchPosition function
+			navigator.geolocation.watchPosition(this.geoSuccess, this.geoError, {enableHighAccuracy: true});
+			// if there is NOT a currently logged in user...
+		} else {
+			alert('You must sign in before tracking location.');
+			console.log('Cannot track user location before log in.');
 		}
 	},
 
