93519fd5bf808193a29c41cd706f6e011edc93ed
Whitney Williams <wweliz@gmail.com>
2014-07-16 00:38:36 -0400

    tabbing over function to line up

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -52,9 +52,9 @@ var AppRouter = Parse.Router.extend({
 
 	////////////////////////////////////////////////////////////////////////
 	// REDIRECT TO SIGN UP IF NO CURRENT USER //////////////////////////////
-  redirectToSignup: function(){
-  	this.navigate('signup', {trigger: true});
-  },
+	redirectToSignup: function(){
+		this.navigate('signup', {trigger: true});
+	},
 
 	////////////////////////////////////////////////////////////////////////
 	// DETERMINING DEVICE SUPPORT FOR GEOLOCATION //////////////////////////
@@ -80,68 +80,64 @@ var AppRouter = Parse.Router.extend({
 		});
 	},
 
-//////////////////////////////////////////////////////////////////////////
-// TRACKING THE CURRENT USER'S LOCATION //////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	// TRACKING THE CURRENT USER'S LOCATION //////////////////////////////////
 
-//there are two methods to get the current position of the device:
-	//navigator.geolocation.getCurrentPosition()
-			//returns the device position ONCE when called
-	//navigator.geolocation.watchPosition()
-			//returns the device position EACH TIME the device position changes
+	//there are two methods to get the current position of the device:
+		//navigator.geolocation.getCurrentPosition()
+				//returns the device position ONCE when called
+		//navigator.geolocation.watchPosition()
+				//returns the device position EACH TIME the device position changes
 
-//both take 3 arguements:
-	// first arg:						success callback [geoSuccess]
-	// second arg: 					failure callback [geoError]
-	// optional third arg:	PositionOptions object
-													// property:	enableHighAccuracy
-													// type: Boolean
-													// default: false
+	//both take 3 arguements:
+		// first arg:						success callback [geoSuccess]
+		// second arg: 					failure callback [geoError]
+		// optional third arg:	PositionOptions object
+														// property:	enableHighAccuracy
+														// type: Boolean
+														// default: false
 
-//////////////////////////////////////////////////////////////////////////
-// DEFINING THE USER LOCATION SUCCESS CALLBACK ///////////////////////////
-// DEFINING THE NEAREST POINT QUERY //////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	// DEFINING THE USER LOCATION SUCCESS CALLBACK ///////////////////////////
+	// DEFINING THE NEAREST POINT QUERY //////////////////////////////////////
 
-geoSuccess: function(position) {
-	var userLatitude = position.coords.latitude;
-	var userLongitude = position.coords.longitude;
-	userGeoPoint = new Parse.GeoPoint({latitude: userLatitude, longitude: userLongitude});
+	geoSuccess: function(position) {
+		var userLatitude = position.coords.latitude;
+		var userLongitude = position.coords.longitude;
+		userGeoPoint = new Parse.GeoPoint({latitude: userLatitude, longitude: userLongitude});
 
-	console.log('Current user location is: latitude:' + userGeoPoint._latitude + ', longitude:'+ userGeoPoint._longitude + '.');
+		console.log('Current user location is: latitude:' + userGeoPoint._latitude + ', longitude:'+ userGeoPoint._longitude + '.');
 
-	//tells the query to look for locations within 10 miles of the user
-	mouseQuery.withinMiles('mouseGeopoint', userGeoPoint, 10);
-	//limits the length of the returned array to 9
-	mouseQuery.limit(9);
-	//finds all objects that match the restraints of the query
-	mouseQuery.find(querySuccess, queryError);
-},
+		//tells the query to look for locations within 10 miles of the user
+		mouseQuery.withinMiles('mouseGeopoint', userGeoPoint, 10);
+		//limits the length of the returned array to 9
+		mouseQuery.limit(9);
+		//finds all objects that match the restraints of the query
+		mouseQuery.find(querySuccess, queryError);
+	},
 
-//queryresults will be an array of objects ordered by distance
-//(nearest to farthest) from the user's location
+	//queryresults will be an array of objects ordered by distance
+	//(nearest to farthest) from the user's location
 
-//////////////////////////////////////////////////////////////////////////
-// DEFINING THE USER LOCATION FAILURE CALLBACK ///////////////////////////
-geoError: function() {
-	alert('Your location could not be determined.');
-	console.log('Your location could not be determined.');
-},
+	//////////////////////////////////////////////////////////////////////////
+	// DEFINING THE USER LOCATION FAILURE CALLBACK ///////////////////////////
+	geoError: function() {
+		alert('Your location could not be determined.');
+		console.log('Your location could not be determined.');
+	},
 
-//////////////////////////////////////////////////////////////////////////
-// DEFINING THE TRACK USER LOCATION FUNCTION /////////////////////////////
-trackUserLocation: function() {
-	// if there is a currently logged in user...
-if ( Parse.User.current() ) {
-		//passes the success and failure callbacks through the watchPosition function
-			navigator.geolocation.watchPosition(this.geoSuccess, this.geoError, {enableHighAccuracy: true});
-	// if there is NOT a currently logged in user...
-} else {
-	console.log('Cannot track user location before log in.');
-	}
-},
-
-
-
-
+	//////////////////////////////////////////////////////////////////////////
+	// DEFINING THE TRACK USER LOCATION FUNCTION /////////////////////////////
+	trackUserLocation: function() {
+		// if there is a currently logged in user...
+	if ( Parse.User.current() ) {
+			//passes the success and failure callbacks through the watchPosition function
+				navigator.geolocation.watchPosition(this.geoSuccess, this.geoError, {enableHighAccuracy: true});
+		// if there is NOT a currently logged in user...
+	} else {
+		console.log('Cannot track user location before log in.');
+		}
+	},
 
 	////////////////////////////////////////////////////////////////////////
 	// RENDERING VIEWS /////////////////////////////////////////////////////
@@ -155,21 +151,21 @@ if ( Parse.User.current() ) {
 
 	renderLogIn: function(){
 		this.swap( new LogInView() );
-  },
+	},
 
 	renderUserHome: function(){
 		trackingPromise.done(function(){
 			if ( Parse.User.current() === null ){
 				this.redirectToSignup();
 			} else {
-		  	//instantiate the UserHomeView with the current user as the model
-		  	this.swap( new UserHomeView({model: Parse.User.current().attributes}) );
+				//instantiate the UserHomeView with the current user as the model
+				this.swap( new UserHomeView({model: Parse.User.current().attributes}) );
 			}
 		}.bind(this));
-  },
+	},
 
-  renderClosestMouse: function(){
-  	trackingPromise.done(function(){
+	renderClosestMouse: function(){
+		trackingPromise.done(function(){
 			if ( Parse.User.current() === null ){
 				this.redirectToSignup();
 			} else {
@@ -177,39 +173,39 @@ if ( Parse.User.current() ) {
 			}
 		}.bind(this));
 
-  },
+	},
 
-  renderSingleMouse: function(){
-  	if ( Parse.User.current() === null ){
+	renderSingleMouse: function(){
+		if ( Parse.User.current() === null ){
 			this.redirectToSignup();
 		} else {
 			this.swap( new MousePhotoView() );
 		}
-  },
+	},
 
-  renderMouseReward: function(){
-  	if ( Parse.User.current() === null ){
+	renderMouseReward: function(){
+		if ( Parse.User.current() === null ){
 			this.redirectToSignup();
 		} else {
 			this.swap( new MouseRewardView() );
 		}
-  },
+	},
 
-  renderMouseGallery: function(){
-  	if ( Parse.User.current() === null ){
+	renderMouseGallery: function(){
+		if ( Parse.User.current() === null ){
 			this.redirectToSignup();
 		} else {
 			this.swap( new MouseGalleryView() );
 		}
-  },
+	},
 
-  renderAllRewards: function(){
-  	if ( Parse.User.current() === null ){
+	renderAllRewards: function(){
+		if ( Parse.User.current() === null ){
 			this.redirectToSignup();
 		} else {
 			this.swap( new AllRewardsView() );
 		}
-  },
+	},
 
 	renderUserSettings: function(){
 		if ( Parse.User.current() === null ){
@@ -217,26 +213,26 @@ if ( Parse.User.current() ) {
 		} else {
 			this.swap( new UserSettingsView() );
 		}
-  },
+	},
 
-  renderMiceHistory: function(){
+	renderMiceHistory: function(){
 		this.swap( new MouseHistoryView() );
-  },
+	},
 
 	renderCongrats: function(){
 		this.swap( new CongratulationsView() );
-  },
+	},
 
-  ////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////
 	// SWAP VIEW FUNCTION //////////////////////////////////////////////////
 	swap: function(view){
-    if (this.currentView)
-    	this.currentView.remove();
-    		//removing a view from the DOM calls stopListening, which stops
-    		//the view from listening to any bound events
-    	this.currentView = view;
-    	this.currentView.render();
-  }
+		if (this.currentView)
+			this.currentView.remove();
+				//removing a view from the DOM calls stopListening, which stops
+				//the view from listening to any bound events
+			this.currentView = view;
+			this.currentView.render();
+	}
 
 });
 
